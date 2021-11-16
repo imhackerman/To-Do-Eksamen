6d557c9f0b837aea3dca7be96741d5513c478ee8
imhackerman <89692186+imhackerman@users.noreply.github.com>
2021-11-16 14:48:41 +0100

    Update server.js
    endret kode i server for å opprette to do oppgaver.
    
    Co-Authored-By: Marina <89974894+marinaj96@users.noreply.github.com>
    Co-Authored-By: elenor99 <40594640+elenor99@users.noreply.github.com>
    Co-Authored-By: raninmj <89643404+raninmj@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { Pool } = require('pg');
 const server = express();
 const PORT = process.env.PORT || 8080;
 server.set('port', PORT);
@@ -12,8 +13,18 @@ server.get("/", function(req, res, next) {
     res.status(200).send("Helo from GET").end();
 });
 
-server.post("/", function(req, res, next) {
-    res.status(200).send("Hello from POST").end();
+server.post("/tasklist", async function(req, res, next) {
+    
+    let userid = 1; // Must be changed when we implement more users than 1.
+
+    let sql = 'INSERT INTO tasklist (id, date, heading, blogtext, userid) VALUES(DEFAULT, DEFAULT, $1, $2, $3) returning *';
+    let values = [updata.heading, updata.tasktext, userid];
+
+    try {
+        let result = await Pool.query(sql, values);
+
+        if (result.rows.length > 0) {}
+    }
 });
 
 server.delete("/", function(req, res, next) {
