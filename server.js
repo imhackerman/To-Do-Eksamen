0693bfe20d2da91c6ef22ab25b813be4f4b645df
Marina <marinaj@uia.no>
2021-11-16 13:32:37 +0100

    Update server.js
    Tester server på port 8080

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const server = express();
+const PORT = process.env.PORT || 8080;
+server.set('port', PORT);
 
+server.listen(server.get('port'), function () {
+    console.log('server running', server.get('port'));
+})
