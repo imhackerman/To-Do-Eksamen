a371998f02c92621561ce28b0ed01900212248e4
imhackerman <89692186+imhackerman@users.noreply.github.com>
2021-11-19 12:52:51 +0100

    Update server.js
    Modulert server js og tasklist.js
    
    Co-Authored-By: Marina <89974894+marinaj96@users.noreply.github.com>
    Co-Authored-By: elenor99 <40594640+elenor99@users.noreply.github.com>
    Co-Authored-By: raninmj <89643404+raninmj@users.noreply.github.com>

--- server.js
+++ server.js
@@ -5,82 +5,13 @@ const server = express();
 const PORT = process.env.PORT || 8080;
 server.set('port', PORT);
 
+const tasklist = require("./modules/tasklist.js");
+
 // MIDDLEWARE ----------------------------
 server.use(express.static("public"));
 server.use(express.json());
 
-// ENDPOINTS --------------------
-
-server.get("/lists", async function(req, res, next){
-    try {
-        let data = await db.getAllTasklists();
-        res.status(200).json(data.rows).end();
-    }catch(err){
-        next(err);
-    }
-})
-
-server.get("/tasks/:id", async function(req, res, next){
-
-    let id = req.params.id;
-
-    try {
-        let data = await db.getTasksFromList(id);
-        res.status(200).send(data.rows).end();
-    }catch(err){
-        console.log(err)
-    }
-})
-
-
-server.get("/tasklist", async function(req, res, next) {
-    try {
-		let data = await db.getAllTasks();
-		res.status(200).json(data.rows).end();
-	} catch (err) {
-		next(err);
-	}
-});
-
-server.post("/tasklist", async function(req, res, next) {
-    
-    let updata = req.body;
-    let userid = 1;
-
-    try {
-        let data = await db.createTask(userid, updata.tasktext, updata.heading);
-
-        if (data.rows.length > 0) {
-            res.status(200).json({msg: "Gjøremålet er lagt til i to do listen!"}).end();
-        }
-        else {
-            throw "Gjøremålet kunne ikke bli lagt til!";
-        }
-    }
-    catch(err) {
-        next(err);
-    }
-});
-
-server.delete("/tasklist", async function(req, res, next) {
-    // method for DELETING from database 
-    let updata = req.body;
-    
-    try{
-        let data = await db.deleteTask(updata.id);
-
-        if(data.rows.length>0){
-            res.status(200).json({msg: "Gjøremålet er blitt slettet!"}).end();
-        }
-        else{
-            throw "Gjøremålet kunne ikke bli slettet!";
-        }
-    }
-    catch(err) {
-        next(err);
-    }
-    
-})
+server.use(tasklist);
 
 //General error handling --------------------------------
 server.use(function (err, req, res, next) {
@@ -90,10 +21,7 @@ server.use(function (err, req, res, next) {
     }).end();
 });
 
-server.post("/tasklist", async function(req, res, next){
-
-})
-
+// start server ------------------------------------------
 server.listen(server.get('port'), function () {
     console.log('server running', server.get('port'));
 });
