1da6049960982018227d63f2d0261db8606122ae
raninmj <ranin.Zz@hotmail.com>
2021-11-23 15:11:12 +0100

    Update db.js
    creating database functions to handle users

--- modules/db.js
+++ modules/db.js
@@ -1,3 +1,4 @@
+const { query } = require("express");
 const pg = require("pg");
 const dbURI = "postgres://dvfrihnwtenmec:dd57911311ad4e4afcced101b316ba1386f75f68d5755b7012cc784e22036d74@ec2-18-202-1-222.eu-west-1.compute.amazonaws.com:5432/d4n071tse3ervn";
 const connstring = process.env.DATABASE_URL || dbURI;
@@ -47,6 +48,36 @@ dbMethods.createTasklist = function(title, userid){
     let values = [title, userid];
     return pool.query(sql, values);
 }
+
+
+//----------------------------------------
+dbMethods.getAllUsers = function() {
+    let sql = "SELECT id, username FROM users";
+    return pool.query(sql); //return the promise 
+
+}
+//--------------------------------------------
+dbMethods.getUser = function(username) {
+    let sql = "SELECT * FROM users WHERE username = $1";
+    let values = [username];
+    return pool.query(sql, values);
+}
+
+//--------------------------------------------
+
+dbMethods.createUser = function(username, password, salt) {
+    let sql = "INSERT INTO users (id, username, password, salt) VALUES(DEFAULT, $1, $2, $3) returning *";
+    let values = [username, password, salt];
+    return pool.query(sql, values);
+}
+
+//---------------------------------------------
+dbMethods.deleteUser = function (id) {
+    let sql = "DELETE FROM users WHERE id = $1 RETURNING *";
+    let values = [id];
+    return pool,query(sql, values);
+}
+
 
 
 module.exports = dbMethods;
