00cb3c7e3a62e286afe59a71ec57eb07bf57e259
Marina <marinaj@uia.no>
2021-11-23 14:11:17 +0100

    Update auth_utils.js
    lagt til funksjoner for å hashe passord, lage token og verifisere token.

--- modules/auth_utils.js
+++ modules/auth_utils.js
@@ -23,15 +23,54 @@ utils.decodeCred = function(credString){
 
 utils.createHash = function(password){
 
+    let hash = {};
+
+    hash.salt = Math.random().toString();
+    hash.value = crypto.scryptSync(password, hash.salt, 64).toString('hex');
+
+    return hash;
+
 }
 
 utils.createToken = function(username, userId){
 
+
+    let part1 = JSON.stringify({'alg': 'HS256', 'typ': 'JWT'});
+    let part2 = JSON.stringify({'user': username, 'userid': userId, 'iat': Date.now()});
+
+    let b64Part1 = Buffer.from(part1).toString('base64');
+    let b64Part2 = Buffer.from(part2).toString('base64');
+
+    let openPart = `${b64Part1}.${b64Part2}`;
+
+    let secret = 'testpassword';
+    let sign = crypto.createHmac('SHA256', secret).updata(openPart).digest('base64');
+
+    return ` ${openPart}.${sign}`
 }
 
 utils.verifyToken = function(token){
 
+    let tokenArr = token.split('.');
+    let openPart = `${tokenArr[0]}.${tokenArr[1]}`;
+    let signToCheck = tokenArr[2];
+
+    let secret = 'testpassword';
+    let sign = crypto.createHmac('SHA256', secret).update(openPart).digest('base64');
+
+    if(signToCheck != sign){
+        return false;
+    }
+
+    let expireTime = payload.iat + 24 * 60 *60 * 1000;
+    if(expireTime < Date.now()){
+        return false;
+    }
+
+    return true;
 }
+
+
 
 
 module.exports = utils;
